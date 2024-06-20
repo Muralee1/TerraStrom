d878a89cc81baee97e08dde9aaf5b10ace775d5b
Muralee1 <144340251+Muralee1@users.noreply.github.com>
2024-06-20 18:38:21 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
